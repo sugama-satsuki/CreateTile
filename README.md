5ce4a378fe10e4c232d7d1a02562ba5e74f69798
sugama-satsuki <piguchi@geolonia.com>
2023-10-01 21:50:39 +0900

    処理を関数化

--- README.md
+++ README.md
@@ -9,19 +9,14 @@ git clone https://github.com/sugama-satsuki/CreateTile.git
 テストデータとして、excelファイルcsvファイルを置いています。
 
 ```
-excel_data
-	└ 12_suisyou_koukyoushisetu-1.xlsx
-
-csv_data
-	└ 470007_hospital.csv
-	└ 473022_evacuation_space.csv
+data
+  └ 12_suisyou_koukyoushisetu-1.xlsx
+  └ 470007_hospital.csv
+  └ 473022_evacuation_space.csv
 ```
 
 ---
-① pmtilesファイルに変換したいexcelファイルを `excel_data` フォルダへ格納
-
-　 ※ データがcsvファイルの場合は `csv_data` へ、geojsonの場合は`gojson_data`へ
-
+① pmtilesファイルに変換したいexcelファイル、csvファイル、geojsonファイルを `data` フォルダへ格納
 
 
 ② ターミナルにて、下記を実行。
--- bin/tile.sh
+++ bin/tile.sh
@@ -3,54 +3,61 @@ set -ex
 
 LOG_ERR=../err_log/`date +%Y-%m-%d_%H-%M-%S.log`
 
+# execlファイルからcsvファイルを生成
+function excelToCsv() {
+    filename=$(basename $1 .xlsx)
+    csv=$filename".csv"
+    xlsx2csv $1 $csv
+}
 
+# csvファイルからgeojsonファイルを生成
+function csvToGeojson() {
+    filename=$(basename $1 .csv)
+    echo "-----"$filename"-----"
+    geojson=$filename".geojson"
+    sed -i '' "s/緯度/lat/" $1
+    sed -i '' "s/経度/lon/" $1
+
+    ogr2ogr -f GeoJSON $geojson $1 -oo X_POSSIBLE_NAMES=lat* -oo Y_POSSIBLE_NAMES=lon*
+    if [ ! -e ../geojson_data ]; then mkdir ../geojson_data ; fi
+}
+
+# geojsonファイルからpmtilesを生成
+function geojsonToTile() {
+    filename=$(basename $1 .geojson)
+    tile=$filename".pmtiles"
+    tippecanoe -o $tile $1
+    if [ ! -e ../tile_data ]; then mkdir ../tile_data ; fi
+}
+
+
+# タイルの生成
 function makeTile(){
 
-    cd ~/Desktop/CreateTile/excel_data
+    cd ~/Desktop/CreateTile/data
 
-    # excelファイルのみ取得し、csvに変換
-    ls *.xlsx | while read line
+    ls | while read line
     do
-        filename=`basename $line .xlsx`
-        csv=$filename".csv"
-        xlsx2csv $line $csv
-        mv $csv ../csv_data
+        extension="."${line##*.}
+        filename=$(basename $line $extension)
+
+        case $extension in
+            ".xlsx") echo "1"$line
+                    excelToCsv $line
+                    csvToGeojson $filename".csv"
+                    break;;
+            ".csv") echo $line
+                    csvToGeojson $line
+                    break;;
+            ".geojson") ;;
+        esac
     done
 
-    cd ../csv_data
-
-    # csvファイルのみ取得し、geojsonに変換
-    ls *.csv | while read line
-    do
-        echo $line
-        sed -i '' "s/緯度/lat/" $line
-        sed -i '' "s/経度/lon/" $line
-        filename=`basename $line .csv`
-        geojson=$filename".geojson"
-        ogr2ogr -f GeoJSON $geojson $line -oo X_POSSIBLE_NAMES=lat* -oo Y_POSSIBLE_NAMES=lon*
-        if [ ! -e ../geojson_data ]; then mkdir ../geojson_data ; fi
-        mv $geojson ../geojson_data
-    done
-
-    cd ../geojson_data
-
-    # geojsonファイルのみ取得し、pmtilesに変換
-    ls *.geojson | while read line
-    do
-        filename=`basename $line .geojson`
-        tile=$filename".pmtiles"
-        tippecanoe -o $tile $line
-        if [ ! -e ../tile_data ]; then mkdir ../tile_data ; fi
-        mv $tile ../tile_data
-        echo $tile
-    done
 
     # TODO：errorログの出力
     # if [[ $2 != "" ]]; then
     #    echo -e "$2" | tee -a $LOG_ERR
     # fi
-
-    # TODO:wihleを関数化
 
 }
 
